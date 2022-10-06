670f5fb61bd63bce7406a474fdc0abc4c8c37dba
Pankaj Gaikar <pankajs.gaikar@gmail.com>
2022-10-07 00:49:58 +0200

    Added player images and widgets.

--- WorldCup 2022 Countdown Widgets/View/Containers/RootContainerView.swift
+++ WorldCup 2022 Countdown Widgets/View/Containers/RootContainerView.swift
@@ -49,8 +49,21 @@ struct RootContainerView: View {
                     }
                     .frame(maxWidth: .infinity, maxHeight: UIScreen.main.bounds.height * 0.30, alignment: .center)
 
+                    Text("Signature Widgets Preview")
+                        .font(.title3)
+                        .bold()
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.5)
+                        .padding()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .foregroundColor(.white)
+                        .background(Color.black.opacity(0.5))
+
+                    WidgetView(imageName: "WorldCup_1024")
+                        .frame(width: UIScreen.main.bounds.width / 2, height: UIScreen.main.bounds.width / 2)
+
                     Text("Countries Widgets Preview - Countries")
-                        .font(.title)
+                        .font(.title3)
                         .bold()
                         .lineLimit(1)
                         .minimumScaleFactor(0.5)
@@ -80,6 +93,39 @@ struct RootContainerView: View {
                         }
                         .padding(.leading)
                     }
+
+                    Text("Players Widgets Preview - Countries")
+                        .font(.title3)
+                        .bold()
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.5)
+                        .padding()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .foregroundColor(.white)
+                        .background(Color.black.opacity(0.5))
+
+                    ScrollView(.horizontal) {
+                        HStack {
+                            ForEach(viewModel.playerNames, id: \.self) { player in
+
+                                VStack {
+                                    WidgetView(imageName: player)
+                                        .frame(width: UIScreen.main.bounds.width / 2, height: UIScreen.main.bounds.width / 2)
+
+                                    Text(player)
+                                        .font(.title2)
+                                        .bold()
+                                        .foregroundColor(.white)
+                                        .padding(.horizontal)
+                                        .padding(.bottom)
+                                }
+                                .background(Color.black.opacity(0.2))
+                                .cornerRadius(10)
+                            }
+                        }
+                        .padding(.leading)
+                    }
+
                     Spacer()
                 }
             }
