6dcc4df1627b9ce82a91ad483194024d3dea5dbe
Pankaj Gaikar <pankajs.gaikar@gmail.com>
2022-10-06 22:43:04 +0200

    Adds background and header, also timer.

---
+++ WorldCup 2022 Countdown Widgets/View/Components/TimeView.swift
@@ -0,0 +1,31 @@
+//
+//  TimeView.swift
+//  WorldCup 2022 Countdown Widgets
+//
+//  Created by Pankaj Gaikar on 06/10/22.
+//
+
+import SwiftUI
+
+struct TimeView: View {
+    let time: String
+    let unit: String
+    
+    var body: some View {
+        VStack {
+            Text(time)
+                .font(.title2)
+                .bold()
+
+            Text(unit)
+                .font(.caption)
+        }
+        .frame(maxWidth: .infinity)
+    }
+}
+
+struct TimeView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimeView(time: "27", unit: "DAYS")
+    }
+}
