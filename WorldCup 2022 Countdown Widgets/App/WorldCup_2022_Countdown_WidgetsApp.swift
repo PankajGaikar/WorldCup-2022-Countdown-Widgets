2905d2a493ebf0ff946ca472ca92b99b96c21a6f
Pankaj Gaikar <pankajs.gaikar@gmail.com>
2022-10-06 20:48:28 +0200

    Setting up Firebase.

--- WorldCup 2022 Countdown Widgets/App/WorldCup_2022_Countdown_WidgetsApp.swift
+++ WorldCup 2022 Countdown Widgets/App/WorldCup_2022_Countdown_WidgetsApp.swift
@@ -6,9 +6,23 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseAnalytics
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        Analytics.setAnalyticsCollectionEnabled(true)
+        return true
+    }
+}
 
 @main
 struct WorldCup_2022_Countdown_WidgetsApp: App {
+
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             RootContainerView()
