ba54b45d3d40a9240ad2101f3599bd42021ef958
Pankaj Gaikar <pankajs.gaikar@gmail.com>
2022-10-16 19:40:33 +0200

    Update analytics to not report redundant.
    Adds actions for app store review and twitter redirect.

--- WorldCup 2022 Countdown Widgets/Analytics/Analytics.swift
+++ WorldCup 2022 Countdown Widgets/Analytics/Analytics.swift
@@ -10,10 +10,33 @@ import FirebaseAnalytics
 
 struct WorldCupAnalytics {
     func reportAnalytics(location: String, data: String) {
-        Analytics.logEvent(location.lowercased().replacingOccurrences(of: " ", with: "_"), parameters: ["data": data])
+        let concatedString = location.lowercased().replacingOccurrences(of: " ", with: "_")
+        let userDefaultsString = concatedString + data.lowercased().replacingOccurrences(of: " ", with: "_")
+        guard hasReportedAlready(report: userDefaultsString) == false else {
+            return
+        }
+        storeReportLocally(report: userDefaultsString)
+        Analytics.logEvent(concatedString, parameters: ["data": data])
     }
 
     func reportError(location: String, error: String) {
         Analytics.logEvent("error", parameters: [location: error])
     }
+
+    func storeReportLocally(report: String) {
+        UserDefaults.standard.setValue(report, forKey: report)
+        UserDefaults.standard.synchronize()
+    }
+
+    func hasReportedAlready(report: String) -> Bool {
+        return UserDefaults.standard.valueExists(forKey: report)
+    }
+}
+
+extension UserDefaults {
+
+    func valueExists(forKey key: String) -> Bool {
+        return object(forKey: key) != nil
+    }
+
 }
